17e9a01ff45213ac2779461a72184e711d813524
lakshmi <lakshmichennur22@gmail.com>
2025-12-30 21:05:56 +0530

    correction3

--- IaaC/main.tf
+++ IaaC/main.tf
@@ -237,7 +237,7 @@ resource "aws_lb" "alb" {
 }
 
 resource "aws_lb_target_group" "frontend_tg" {
-  name        = substr("${local.name_prefix}-frontend", 0, 16)
+  name        = regexreplace(substr(local.name_prefix, 0, 20), "-+$", "")
   port        = 3000
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
@@ -245,7 +245,7 @@ resource "aws_lb_target_group" "frontend_tg" {
 }
 
 resource "aws_lb_target_group" "backend_tg" {
-  name        = substr("${local.name_prefix}-backend", 0, 16)
+  name        = regexreplace(substr(local.name_prefix, 0, 20), "-+$", "")
   port        = 8080
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
